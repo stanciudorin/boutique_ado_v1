abca8b64cd90145943b7030470711738ef9724e5
Dorin Stanciu <stanciudorin@yahoo.com>
2020-10-25 18:30:24 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HgDaELfvgQw87oVo9bLf2WVPUl1IcAf5QnKMCPHUpmyuv8xUo0bRqUHIviRpmPzM1iTtIf1alDRTERN62D2sQfK00QyWbzcDo',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
