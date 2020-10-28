14509e53cb44d7c6f5382391ca80febfec4b3f4a
Dorin Stanciu <stanciudorin@yahoo.com>
2020-10-28 21:40:50 +0000

    Completed webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -1,9 +1,7 @@
 import uuid
-
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-
 from products.models import Product
 
 
@@ -22,6 +20,9 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
 
     def _generate_order_number(self):
         """
@@ -29,27 +30,30 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
+
     def update_total(self):
         """
-        Update grand total each time a line item is added,
-        accounting for delivery costs.
+            Update grand total each time a line item is added,
+            accounting for delivery costs.
         """
+
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
-        self.grand_total = self.order_total + self.delivery_cost
-        self.save()
+            self.grand_total = self.order_total + self.delivery_cost
+            self.save()
+
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number
-        if it hasn't been set already.
+            Override the original save method to set the order number
+            if it hasn't been set already.
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
-        super().save(*args, **kwargs)
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return self.order_number
@@ -62,13 +66,15 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
+
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
-        and update the order total.
+            Override the original save method to set the lineitem total
+            and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
+
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
