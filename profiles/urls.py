baab41cf8300baaa2eba505e8b4ea87b591c3124
Dorin Stanciu <stanciudorin@yahoo.com>
2020-10-31 21:38:32 +0000

    046 Profiles part 6 - completed confirmation emails

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.profile, name='profile')
-    ath('order_history/<order_number>', views.order_history, name='order_history'),
+    path('', views.profile, name='profile'),
+    path('order_history/<order_number>', views.order_history, name='order_history'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,8 +4,7 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
-from checkout import Order
-
+from checkout.models import Order
 
 def profile(request):
     """ Display the user's profile. """
@@ -28,18 +27,20 @@ def profile(request):
     }
 
     return render(request, template, context)
-    
-    def order_history(request, order_number):
-        order = get_object_or_404(Order, order_number)
 
-        message.info(request,(
-            f'This is a past confirmation for order number {{order_number}}. A confirmation email was sent on the order date'
-        ))
 
-        template = 'checkout/checkout/success.html'
-        context = {
-            "order": order,
-            "from_profile": True,
-        }
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
 
-        return render(request, template, context)
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
